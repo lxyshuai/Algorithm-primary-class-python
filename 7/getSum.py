4636d9dd7acd3ff219fda111effe655b2d9f3e74
TENCENT\kobexyluo <kobexyluo@tencent.com>
2018-09-11 10:15:24 +0800

    get_sum_dp

--- 7/getSum.py
+++ 7/getSum.py
@@ -1,9 +1,28 @@
+# coding=utf-8
 def get_sum_recursive(array, aim):
     if array:
         return process(array, 0, 0, aim)
 
 
 def get_sum_dp(array, aim):
+    i = len(array)
+    j = sum(array) + 1
+    if aim > j - 1:
+        return False
+    dp = [[False for _ in range(j)] for _ in range(i)]
+    # 最后一行为False
+    for _j in range(j):
+        dp[i - 1][_j] = False
+    # sum==aim and _i = i -1 => True
+    dp[i - 1][aim] = True
+
+    for _i in reversed(range(i - 1)):
+        for _j in range(j):
+            if _j + array[_i] > j - 1:
+                dp[_i][_j] = dp[_i + 1][_j]
+            else:
+                dp[_i][_j] = dp[_i + 1][_j] or dp[_i + 1][_j + array[_i]]
+    return dp[0][aim]
 
 
 def process(array, i, sum, aim):
@@ -17,3 +36,4 @@ def process(array, i, sum, aim):
 if __name__ == '__main__':
     a = [1, 2, 3]
     print get_sum_recursive(a, 7)
+    print get_sum_dp(a, 4)
